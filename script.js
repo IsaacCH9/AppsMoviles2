7dcec78d9ba128639b3bcfc73cbcb71a2fb65be7
Chimal <carbajalchimalisaac0@gmail.com>
2024-09-18 23:17:25 -0600

    Primer respositorio

---
+++ script.js
@@ -0,0 +1,46 @@
+// 18/09/2024 /Carbajl Chimal Isaac
+
+//comentario en una sola linea
+
+/*comentario
+multi
+lineas
+*/
+
+//VARIABLES
+/*
+Las varibles guardan informacion que puedes reutilizar
+ */
+
+//Dedlaracion de variables
+
+let nombre = "Isaac";
+
+const PI = 3.14159287;
+
+var Edad = 25;
+
+/*Tipos de datos:
+cadenas de datos (String):"HOLA MUNDO"
+Numero (number): 5,4,14
+Boleano (Boolean): true, false
+*/
+
+//Ejemplo:
+
+let mensaje = "hola mundo";
+let Numero = 42;
+let estudiante = true;
+
+//Operadores matematicos
+//igualdad
+let a = 5;
+let b = 2;
+
+let suma = a+b;//7
+let resta = a-b;//3
+let multiplicacion = a*b;
+let division = a/b;
+
+//git add . / git commit -m "18/09/2024 Isaac"
+//git push
